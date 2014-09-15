fab723aa7504ddd3c4d0c5654eeee84976525ec7
Danilo Sanchi <danilo.sanchi@gmail.com>
2014-09-15 18:31:28 +0200

    added metadata entity

---
+++ src/Dan/MainBundle/Entity/Metadata.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Dan\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Metadata
+ *
+ * @ORM\Table(name="dan_metadata")
+ * @ORM\Entity(repositoryClass="Dan\MainBundle\Entity\Repository\MetadataRepository")
+ */
+class Metadata
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="context", type="string", length=255)
+     */
+    private $context;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="json_array", nullable=true)
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set context
+     *
+     * @param string $context
+     * @return UserMetadata
+     */
+    public function setContext($context)
+    {
+        $this->context = $context;
+
+        return $this;
+    }
+
+    /**
+     * Get context
+     *
+     * @return string 
+     */
+    public function getContext()
+    {
+        return $this->context;
+    }
+
+    /**
+     * Set content
+     *
+     * @param array $content
+     * @return UserMetadata
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return array 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
---
+++ src/Dan/MainBundle/Tests/Model/MetadataTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Dan\MainBundle\Tests\Model;
+
+use Dan\CoreBundle\Test\WebTestCase;
+
+class MetadataManagerTest extends WebTestCase
+{
+
+    public function getFixturesToLoad()
+    {
+        return array(
+        );
+    }
+
+    public function test_getMetadataWhenMetadataDoNotExist()
+    {
+        $container = $this->getContainer();
+        $metadataManager = $container->get('model.manager.metadata');
+
+        $data = $metadataManager->getMetadata('test');
+        $this->assertNull($data);
+
+        $data = $metadataManager->getMetadata('test','a.path');
+        $this->assertNull($data);
+
+        $data = $metadataManager->getMetadata('test','a.path', 'aDefaultValue');
+        $this->assertEquals('aDefaultValue', $data);
+
+        $data = $metadataManager->getMetadata('test',null , 'aDefaultValue');
+        $this->assertEquals('aDefaultValue', $data);
+
+        $data = $metadataManager->getMetadata('test',null , 'aDefaultValue', array('Default' => 'Default2'));
+        $this->assertEquals('aDefault2Value', $data);
+
+        $data = $metadataManager->getMetadata('test',null , array('A1', 'A2'), array('A' => 'B'));
+        $this->assertEquals(array('B1', 'B2'), $data);
+
+    }
+
+    public function test_getMetadataWhenMetadataExists()
+    {
+        $container = $this->getContainer();
+        $metadataManager = $container->get('model.manager.metadata');
+
+        $metadataManager->setMetadata('test', null, 'aValue');
+        
+        $data = $metadataManager->getMetadata('test');
+        $this->assertEquals('aValue', $data);
+
+        $data = $metadataManager->getMetadata( 'test', null, 'aDefaultValue');
+        $this->assertEquals('aValue', $data);
+
+        $metadataManager->setMetadata('test', null, array(
+            'A' => array(
+                'AA' => 1,
+                'AB' => 2,
+            ),
+            'B' => 3
+        ));
+
+        $this->assertEquals(1, $metadataManager->getMetadata('test', 'A.AA'));
+        $this->assertEquals(2, $metadataManager->getMetadata('test', 'A.AB'));
+        $this->assertEquals(3, $metadataManager->getMetadata('test', 'B'));
+        $this->assertEquals(array('AA' => 1, 'AB' => 2,), $metadataManager->getMetadata('test', 'A'));
+
+
+    }
+    
+}
